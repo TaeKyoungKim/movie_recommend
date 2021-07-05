332c891759709941d6f22704126ca76ef5d8e5ac
goajang <goajang@naver.com>
2021-07-05 16:36:26 +0900

    get_user_list function

--- crawring.py
+++ crawring.py
@@ -35,6 +35,29 @@ def genre_list(url):
       # print(genre.a.get_text())
   return genre_list
 
-url = "http://movie.naver.com/movie/point/af/list.nhn"
-genre_list_data = genre_list(url)
-print(genre_list_data)
+# url = "http://movie.naver.com/movie/point/af/list.nhn"
+# genre_list_data = genre_list(url)
+# print(genre_list_data)
+
+def get_user_list(url):
+  res = requests.get(url)
+  content = res.text
+
+  soup = BeautifulSoup(content, 'html5lib')
+
+  page_links = soup.select('a[href]')
+  page_link_list = []
+
+  for link in page_links:
+    if re.search(r'&target=after', link['href']):
+      target_url='http://movie.naver.com'+str(link['href'])
+      page_link_list.append(target_url)
+  if len(page_link_list) !=1:
+    pop_number = len(page_link_list)-1
+    page_link_list.pop(pop_number)
+
+  return page_link_list
+
+# url = "http://movie.naver.com/movie/point/af/list.nhn?st=mcode&sword=187322&target=after"
+# point_data = get_user_list(url)
+# print(point_data)
